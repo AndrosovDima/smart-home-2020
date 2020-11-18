b3b0e3253c973512519a73b03954be7cd5162ee9
Androsov Dmitri <Androsov.DYa@phystech.edu>
2020-11-18 14:52:59 +0300

    Task_4 Corrected

--- src/main/java/ru/sbt/mipt/oop/main/EventHandlerAdapter.java
+++ src/main/java/ru/sbt/mipt/oop/main/EventHandlerAdapter.java
@@ -3,19 +3,23 @@ package ru.sbt.mipt.oop.main;
 import com.coolcompany.smarthome.events.CCSensorEvent;
 import com.coolcompany.smarthome.events.APIEventHandler;
 
+import java.util.Map;
+
 public class EventHandlerAdapter implements APIEventHandler {
 
     private final SmartHome smartHome;
     private final EventHandler eventHandler;
+    private final Map<String, SensorEventType> mapForConvertingCCSensorEventsToSensorEvents;
 
-    public EventHandlerAdapter(SmartHome smartHome, EventHandler eventHandler) {
-        this.eventHandler = eventHandler;
+    public EventHandlerAdapter(SmartHome smartHome, EventHandler eventHandler, Map<String, SensorEventType> mapForConvertingCCSensorEventsToSensorEvents) {
         this.smartHome = smartHome;
+        this.eventHandler = eventHandler;
+        this.mapForConvertingCCSensorEventsToSensorEvents = mapForConvertingCCSensorEventsToSensorEvents;
     }
 
     @Override
     public void handleEvent(CCSensorEvent event) {
-        SensorEvent sensorEvent = new SensorEventAdapter(event).getSensorEvent();
+        SensorEvent sensorEvent = new SensorEventAdapter(event, mapForConvertingCCSensorEventsToSensorEvents).getSensorEvent();
         eventHandler.manage(smartHome, sensorEvent);
     }
 }
--- src/main/java/ru/sbt/mipt/oop/main/MyConfiguration.java
+++ src/main/java/ru/sbt/mipt/oop/main/MyConfiguration.java
@@ -5,37 +5,58 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @ComponentScan
 public class MyConfiguration {
 
     @Bean
-    public SmartHome getSmartHome(SmartHomeGettable smartHomeGettable){
+    public SmartHome smartHome(SmartHomeGettable smartHomeGettable){
         return smartHomeGettable.loadHome();
     }
 
     @Bean
-    public SensorEventsManager getEventHandler(List<EventHandler> managers, SmartHome smartHome){
+    public SensorEventsManager eventHandler(Collection<EventHandlerAdapter> eventAdapters){
         SensorEventsManager sensorEventsManager = new SensorEventsManager();
-        managers.stream().map(eh -> new EventHandlerAdapter(smartHome, eh))
-                .forEach(sensorEventsManager::registerEventHandler);
+        eventAdapters.forEach(sensorEventsManager::registerEventHandler);
         return sensorEventsManager;
     }
 
     @Bean
-    public EventHandler getLightEventManager(){
-        return new DecoratorAlarm(new DecoratorSMS(new LightEventManager()));
+    public EventHandlerAdapter lightEventManager(SmartHome smartHome){
+        return new EventHandlerAdapter(smartHome, new DecoratorAlarm(new DecoratorSMS(new LightEventManager())),
+                mapForConvertingCCSensorEventsToSensorEvents());
     }
 
     @Bean
-    public EventHandler getDoorEventManager(){
-        return new DecoratorAlarm(new DoorEventManager());
+    public EventHandlerAdapter doorEventManager(SmartHome smartHome){
+        return new EventHandlerAdapter(smartHome, new DecoratorAlarm(new DoorEventManager()),
+                mapForConvertingCCSensorEventsToSensorEvents());
     }
 
     @Bean
-    public EventHandler getHallDoorEventManager(){
-        return new DecoratorAlarm(new HallDoorEventManager());
+    public EventHandlerAdapter hallDoorEventManager(SmartHome smartHome){
+        return new EventHandlerAdapter(smartHome, new DecoratorAlarm(new HallDoorEventManager()),
+                mapForConvertingCCSensorEventsToSensorEvents());
+    }
+
+    @Bean
+    public EventHandlerAdapter signalingHandlerAdapter(AlarmManager eventHandler, SmartHome smartHome){
+        return new EventHandlerAdapter(smartHome, eventHandler, mapForConvertingCCSensorEventsToSensorEvents());
+    }
+
+    @Bean
+    public Map<String, SensorEventType> mapForConvertingCCSensorEventsToSensorEvents() {
+        return new HashMap<>(
+                Map.of("LightIsOn", SensorEventType.LIGHT_ON,
+                        "LightIsOff", SensorEventType.LIGHT_OFF,
+                        "DoorIsOpen", SensorEventType.DOOR_OPEN,
+                        "DoorIsClosed", SensorEventType.DOOR_CLOSED,
+                        "DoorIsLocked", SensorEventType.ALARM_ACTIVATE,
+                        "DoorIsUnlocked", SensorEventType.ALARM_DEACTIVATE)
+        );
     }
 }
